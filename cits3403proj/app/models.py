b0c34ae1d42ed23afa343384adc1ab5429660464
Lawrence Follis <22236729@student.uwa.edu.au>
2020-05-17 18:23:59 +0800

    added 'quiz' and 'attempt' tables to models

--- cits3403proj/app/models.py
+++ cits3403proj/app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from flask_login import UserMixin
+from datetime import datetime
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -11,3 +12,15 @@ class User(db.Model):
   def __repr__(self): 
     return f"User('{self.username}', '{self.email}')"
   
+class Quiz(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  category = db.Column(db.String(32), index=True, unique=True)
+  filename = db.Column(db.String(32),unique=True)
+  date_added = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+class Attempt(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'))
+  score = db.Column(db.Integer, index=True)
+  date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
